9f11a6356485bb4e0aa9f6e6cbf13dba9b8ad2e7
mandareis <amanda@depaula.xyz>
2021-03-31 15:51:40 -0700

    deletes message on server

--- server/app.ts
+++ server/app.ts
@@ -2,9 +2,9 @@ import express from "express";
 
 const app = express();
 const port = 3000;
-app.get("/", (req, res) => {
-  res.send("The sedulous hyena ate the antelope!");
-});
+// app.get("/", (req, res) => {
+//   res.send("It's working!");
+// });
 app.listen(port, () => {
   return console.log(`server is listening on ${port}`);
 });
